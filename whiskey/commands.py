8e8befb774c308e53d41bc0d16d6a4e0f2bd78d1
Nick Wynja <nick@nickwynja.com>
2019-11-17 18:45:36 -0500

    Adds WATCH_FILES setting for reload

--- whiskey/commands.py
+++ whiskey/commands.py
@@ -1,6 +1,7 @@
 from whiskey import app, tasks, flatpages, helpers
 from fabric.tasks import execute
 import click
+import os
 
 
 @app.cli.command()
@@ -45,6 +46,9 @@ def reload():
     """Runs a livereload server"""
     from livereload import Server
     server = Server(app.wsgi_app)
+    if 'WATCH_FILES' in app.config:
+        for f in app.config['WATCH_FILES']:
+            server.watch(os.path.expanduser(f))
     server.watch('%s/resume.md' % app.config['CONTENT_PATH'],
                  tasks.generate_resume_pdf)
     server.watch(app.config['CONTENT_PATH'])
