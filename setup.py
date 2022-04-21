b02dd217f5eb536a5a37aade57ae723c610038eb
Nick Wynja <nick@nickwynja.com>
2022-04-21 08:07:24 -0500

    bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='whiskey-flask',
-    version='0.3.6',
+    version='0.3.7',
     author="Nick Wynja",
     author_email="nick@nickwynja.com",
     description="Whiskey makes writing happen.",
